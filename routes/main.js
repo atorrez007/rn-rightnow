f2572201081cfb7dfb99277952e9352549f59cb3
funkee <xxtrm98@protonmail.com>
2023-02-22 17:44:03 -0800

    added preliminary router param

--- routes/main.js
+++ routes/main.js
@@ -5,9 +5,32 @@ const router = require("express").Router();
 const rawHospitalData = fs.readFileSync("test-hospital-data.json", "utf-8");
 const hospitalObj = JSON.parse(rawHospitalData);
 
-router.get("/home", (req, res) => {
+// create an endpoint to generate data and import into MongoDB via Mongoose.
+
+// create hospital id router param
+router.param("hospital", function (req, res, next, ID) {
+  const hospitalArray = Object.values(hospitalObj);
+
+  req.hospitals = hospitalArray;
+
+  req.hospital = req.hospitals.find(
+    (hospital) => hospital["Facility ID"] === ID
+  );
+
+  next();
+});
+
+router.get("/hospitals", (req, res) => {
   // console.log(hospitalObj.length);
   res.send(hospitalObj);
+});
+
+router.get("/hospitals/:hospital", (req, res) => {
+  if (req.hospital) {
+    res.send(req.hospital);
+  } else {
+    res.send("Hospital not found.");
+  }
 });
 
 module.exports = router;
